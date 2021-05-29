f4829e2808e0c7190ad98109f5f809f917fc4515
mdarwinj382000 <mdarwinj@gmail.com>
2021-05-29 17:16:36 +0530

    HackerRankSolved

--- src/labs/djm/hackerrank/InherintanceI.java
+++ src/labs/djm/hackerrank/InherintanceI.java
@@ -1,7 +1,25 @@
 package labs.djm.hackerrank;
 
-public class InherintanceI {
-    public static void main(String[] args) {
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
 
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+class InherintanceI {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
     }
 }
