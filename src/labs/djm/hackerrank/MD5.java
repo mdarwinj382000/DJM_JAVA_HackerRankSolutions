beeed77ad37e29c38dc54ed6b79474bbbbb60c43
mdarwinj382000 <mdarwinj@gmail.com>
2021-06-07 23:29:06 +0530

    HackerRankSolved

--- src/labs/djm/hackerrank/MD5.java
+++ src/labs/djm/hackerrank/MD5.java
@@ -1,7 +1,22 @@
 package labs.djm.hackerrank;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 public class MD5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        MessageDigest md=MessageDigest.getInstance("MD5");
+        byte[] message=md.digest(str.getBytes());
+        BigInteger no=new BigInteger(1, message);
+        StringBuilder hashtext = new StringBuilder(no.toString(16));
+        while (hashtext.length() < 32) {
+            hashtext.insert(0, "0");
+        }
+        System.out.println(hashtext);
 
     }
 }
--- src/labs/djm/hackerrank/SHA256.java
+++ src/labs/djm/hackerrank/SHA256.java
@@ -1,7 +1,21 @@
 package labs.djm.hackerrank;
 
-public class SHA256 {
-    public static void main(String[] args) {
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
+public class SHA256 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] message = md.digest(str.getBytes());
+        BigInteger no = new BigInteger(1, message);
+        StringBuilder hashtext = new StringBuilder(no.toString(16));
+        while (hashtext.length() < 32) {
+            hashtext.insert(0, "0");
+        }
+        System.out.println(hashtext);
     }
 }
